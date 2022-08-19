c7df218a0398caafcf19fc820d0d05fecae07950
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-19 10:31:07 -0700

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,48 @@
+# Marvish Chandra
+
+class MyCouponSaver:
+   attr_reader :Annual
+   attr_reader : January
+   attr_reader : February
+   attr_reader : March
+   attr_reader : April
+   attr_reader : May
+   attr_reader : June
+   attr_reader : July
+   attr_reader : August
+   attr_reader : September
+   attr_reader : October
+   attr_reader : November
+   attr_reader : December
+
+  
+      def initialize(Annual)
+        @annual = Annual
+        @january = January
+        @february = February
+        @march = March
+        @april = April
+        @may = May 
+        @june = June
+        @july = July
+        @august = August
+        @september = September
+        @october = October
+        @november = November
+        @december = December
+      end
+
+
+  # Food Services (On Demand)
+  def GrubHub(self):
+      
+  
+  def Postmates(self):
+      
+  
+  def DoorDash(self):
+  def PizzaHut(self):
+  def Domino(self):
+      
+  
+  
